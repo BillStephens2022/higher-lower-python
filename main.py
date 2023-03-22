2a11ee535246da486ac53c412ca26c35a2fca8d3
BillStephens2022 <two4onebill@yahoo.com>
2023-03-22 10:20:17 -0400

    added function to check answer

--- main.py
+++ main.py
@@ -11,10 +11,23 @@ def pick_random():
     print(f"Compare A: {pick_1['name']}, a {pick_1['description']}, from {pick_1['country']}")
     print(vs)
     print(f"Compare B: {pick_2['name']}, a {pick_2['description']}, from {pick_2['country']}")
+    if pick_1['follower_count'] > pick_2['follower_count']:
+        result = 'A'
+    else:
+        result = 'B'
     
+    return result
+
+def check_choice(user_choice, answer):
+    if user_choice == answer:
+        return "you are right"
+    else:
+        return "you are wrong"
+
+
 
 print(logo)
-pick_random()
+answer = pick_random()
 user_choice = (input("Who has more followers?  Type 'A' or 'B': ")).upper()
 print(user_choice)
-
+print(check_choice(user_choice, answer))
